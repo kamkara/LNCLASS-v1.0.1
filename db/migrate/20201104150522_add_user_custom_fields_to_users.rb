f0f1b5b7bace22e04722b31bf88b98b4f8db1676
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-11-04 15:06:43 +0000

    add  Custom fields to User

---
+++ db/migrate/20201104150522_add_user_custom_fields_to_users.rb
@@ -0,0 +1,14 @@
+class AddUserCustomFieldsToUsers < ActiveRecord::Migration[6.0]
+   def change
+    add_column :users, :username, :string
+    add_column :users, :city, :string
+    add_column :users, :school_name, :string
+    add_column :users, :school_code, :string
+    add_column :users, :level_id, :string
+    add_column :users, :contact, :string
+    add_column :users, :matricule, :string
+    add_column :users, :role, :string
+    add_column :users, :memo, :string
+    add_column :users, :slug, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_145129) do
+ActiveRecord::Schema.define(version: 2020_11_04_150522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 2020_11_04_145129) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "city"
+    t.string "school_name"
+    t.string "school_code"
+    t.string "level_id"
+    t.string "contact"
+    t.string "matricule"
+    t.string "role"
+    t.string "memo"
+    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
