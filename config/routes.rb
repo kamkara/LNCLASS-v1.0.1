4699e47597bce8cac8d5eac6ffd535a23c780d5f
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-11-05 11:31:49 +0000

    add level

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   resources :levels
-=======
   resources :materials
->>>>>>> Material
   resources :courses
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  root to:'home#index'
+  root to:'levels#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
